cdf6703fcf25570b3ffe77c44cd086a65d716098
wangbenqi <benqi@lizidata.com>
2020-06-18 18:26:19 +0800

    ccks2020

--- NER/CCKS_2017/src/del_no_label_data.py
+++ NER/CCKS_2017/src/del_no_label_data.py
@@ -11,7 +11,7 @@ for i in f:
     if len(set(l)) > 1:
         new.append(i)
         print(i)
-        
+
 write_jsonline("/Users/ouhon/PycharmProjects/keras_nlp_tutorial/NER/CCKS_2017/data/raw_data/data2.jsonl", new)
 print(len(f))
 print(len(new))
